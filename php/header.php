35c7b33b878a3ff0849177abd38332375d63750f
Mateo Villablanca <167587307+WhiteMooncy@users.noreply.github.com>
2025-10-29 15:28:01 -0300

    update

---
+++ php/header.php
@@ -0,0 +1,10 @@
+<header>
+    <h1 class="headerLogo">Hydro-Conecta</h1>
+    <nav class="headerNav">
+        <ul>
+            <li class="headerNavBlock"><a href="../index.html" class="headerNavLinks">Inicio</a></li>
+            <li class="headerNavBlock"><a href="../pages/nosotros.php" class="headerNavLinks">Nosotros</a></li>
+            <li class="headerNavBlock"><a href="#" class="headerNavLinks">Contacto</a></li>
+        </ul>
+    </nav>
+</header>
